fda75767c109b76c48cb0c0dda861ea71153ec34
root <root@ip-172-31-15-40.us-east-2.compute.internal>
2021-03-02 11:35:16 +0000

    Ticket nummber: useradd syntax issue

--- useradd.sh
+++ useradd.sh
@@ -1,16 +1,9 @@
-#sudo useradd -d /home/$username1  -m $username1
-
-#sudo useradd -d /home/$username2 -m $username2
-
-#sudo useradd $username2
-
-#sudo echo "username1123" | passwd --stdin $username1
-#sudo echo "username2123" | passwd --stdin $username2
 
 
 
 echo "------Package installation-----"
 
+packagename=$1
 
-yum install $packagename -y 
+sudo yum install $packagename -y 
 
