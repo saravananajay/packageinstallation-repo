201f316bb34de74f9509c3f72cb766a499c995af
root <root@ip-172-31-15-40.us-east-2.compute.internal>
2021-03-02 11:24:23 +0000

    Ticket nummber: useradd syntax issue

--- useradd.sh
+++ useradd.sh
@@ -2,5 +2,5 @@ sudo useradd $username1
 
 sudo useradd $username2
 
-sudo echo "username1123" | passwd --stdin $username1
-sudo echo "username2123" | passwd --stdin $username2
+#sudo echo "username1123" | passwd --stdin $username1
+#sudo echo "username2123" | passwd --stdin $username2
